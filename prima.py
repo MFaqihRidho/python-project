43436fb6c5517f5d9d44c74794250d89ab452dd0
M.Faqih Ridho <74019017+MFaqihRidho@users.noreply.github.com>
2020-12-15 20:06:23 +0700

    Update prima.py

--- prima.py
+++ prima.py
@@ -1,14 +1,52 @@
-nomor = int(input("Nomor Berapa"))
+nomor = int(input("Nomor Berapa: "))
+
+def dot():
+    global nomor
+    global num
+    if len(nomor) > 3:
+        num.insert(-3,".")
+        if len(nomor) > 6:
+            num.insert(-7,".")
+            if len(nomor) > 9:
+                num.insert(-11,".")
+
+def dot2():
+    global kali
+    global kal
+    if len(kali) > 3:
+        kal.insert(-3,".")
+        if len(kali) > 6:
+            kal.insert(-7,".")
+            if len(kali) > 9:
+                kal.insert(-11,".")
 
 
 
 if nomor > 1:
     for i in range(2,nomor):
         if (nomor % i == 0):
+            kali = nomor // i
+            nomor = str(nomor)
+            num = list(nomor)
+            kali = str(kali)
+            kal = list(kali)
+            dot()
+            dot2()
+            n = "".join(num)
+            k = "".join(kal)
             print("Bukan bilangan prima")
-            print(i,"kali",nomor // i,"=", nomor)
+            print(i,"kali",k,"=", n)
             break
     else:
-        print(nomor," adalah bilangan prima")
+        nomor = str(nomor)
+        num = list(nomor)
+        dot()
+        n = "".join(num)
+        print(n," adalah bilangan prima")
 else:
     print("Bukan bilangan prima")
+
+
+
+
+
